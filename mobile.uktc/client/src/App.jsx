b9a20485ba4a38d3440b52a30487f1e12063a309
Boyan Gyurov <bobigurov36@gmail.com>
2024-08-28 10:26:35 +0300

    Add responsive view for register page

--- mobile.uktc/client/src/App.jsx
+++ mobile.uktc/client/src/App.jsx
@@ -1,158 +1,174 @@
-import { useState } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import { AuthContext } from './contexts/AuthContext'
-import { Alert, Snackbar } from '@mui/material';
+import { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { AuthContext } from "./contexts/AuthContext";
+import { Alert, Snackbar } from "@mui/material";
 
-import { authServiceFactory } from './services/authService'
-import { Navigation } from './components/Navigation/Navigation'
-import { Hero } from './components/Hero/Hero'
-import { Login } from './components/Login/Login'
-import { Logout } from './components/Logout/Logout'
+import { authServiceFactory } from "./services/authService";
+import { Navigation } from "./components/Navigation/Navigation";
+import { Hero } from "./components/Hero/Hero";
+import { Login } from "./components/Login/Login";
+import { Logout } from "./components/Logout/Logout";
 
-import { Register } from './components/Register/Register.jsx'
-import { CreateAd } from './components/CreateAd/CreateAd.jsx'
-import { Catalog } from './components/Catalog/Catalog.jsx'
-import { Search } from './components/Search/Search.jsx'
-import { AdDetails } from './components/AdDetails/AdDetails.jsx'
-import { EditAd } from './components/EditAd/EditAd.jsx'
-import Profile from './components/Profile/Profile.jsx';
-import usePersistedState from './hooks/usePersistantState.js';
-import AuthGuard from './guards/AuthGuard.jsx';
-import NotFound from './components/NotFound/NotFound.jsx';
-import { Footer } from './components/Footer/Footer.jsx';
-import ProfileGuard from './guards/ProfileGuard.jsx';
-import { AdsProvider } from './contexts/AdsContext.jsx';
-
+import { Register } from "./components/Register/Register.jsx";
+import { CreateAd } from "./components/CreateAd/CreateAd.jsx";
+import { Catalog } from "./components/Catalog/Catalog.jsx";
+import { Search } from "./components/Search/Search.jsx";
+import { AdDetails } from "./components/AdDetails/AdDetails.jsx";
+import { EditAd } from "./components/EditAd/EditAd.jsx";
+import Profile from "./components/Profile/Profile.jsx";
+import usePersistedState from "./hooks/usePersistantState.js";
+import AuthGuard from "./guards/AuthGuard.jsx";
+import NotFound from "./components/NotFound/NotFound.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import ProfileGuard from "./guards/ProfileGuard.jsx";
+import { AdsProvider } from "./contexts/AdsContext.jsx";
 
 function App() {
-    const navigate = useNavigate()
-    const [auth, setAuth] = usePersistedState('auth', {})
-    const [successMsg, setSuccessMsg] = useState()
-    const [error, setError] = useState()
-    const authSevice = authServiceFactory(auth.accessToken)
+  const navigate = useNavigate();
+  const [auth, setAuth] = usePersistedState("auth", {});
+  const [successMsg, setSuccessMsg] = useState();
+  const [error, setError] = useState();
+  const authSevice = authServiceFactory(auth.accessToken);
 
-    const handleCloseMsg = () => {
-        setSuccessMsg('');
-        setError('')
-    };
+  const handleCloseMsg = () => {
+    setSuccessMsg("");
+    setError("");
+  };
 
-    const onLoginSubmit = async (data) => {
-        try {
-            const result = await authSevice.login(data)
-            
-            setAuth(result)
-            navigate('/')
-            setSuccessMsg(`Добре дошъл отново , ${result.username}!`)
-        } catch (error) {
-            //TODO Error
-            setError(error.message)
-        }
+  const onLoginSubmit = async (data) => {
+    try {
+      const result = await authSevice.login(data);
 
+      setAuth(result);
+      navigate("/");
+      setSuccessMsg(`Добре дошъл отново , ${result.username}!`);
+    } catch (error) {
+      //TODO Error
+      setError(error.message);
     }
-    const onRegisterSubmit = async (values) => {
-        const { confirmPass, ...registerData } = values
-        if (confirmPass !== registerData.password) {
-
-            setError('Моля, уверете се, че полетата за парола и за потвърждение на паролата съвпадат')
-            return
-        }
-
-        if (registerData.password.length < 5) {
-            setError('Паролата трябва да е повече от 5 символа')
-            return
-        }
-
-        try {
-            const result = await authSevice.register(registerData)
-
-            setAuth(result)
-            navigate('/')
-            setSuccessMsg(`Успешна регистрация , Добре дошли!`)
-        } catch (error) {
-            setError(error.message)
-
-        }
+  };
+  const onRegisterSubmit = async (values) => {
+    const { confirmPass, ...registerData } = values;
+    if (confirmPass !== registerData.password) {
+      setError(
+        "Моля, уверете се, че полетата за парола и за потвърждение на паролата съвпадат"
+      );
+      return;
     }
 
-    const onLogout = async () => {
-        try {
-            await authSevice.logout();
-            setAuth({})
-        } catch (error) {
-            setAuth({})
-        }
-
+    if (registerData.password.length < 5) {
+      setError("Паролата трябва да е повече от 5 символа");
+      return;
     }
-    const context = {
-        onLoginSubmit,
-        onRegisterSubmit,
-        onLogout,
-        userId: auth._id,
-        token: auth.accessToken,
-        userEmail: auth.email,
-        userName: auth.username,
-        isAuthenticated: !!auth.accessToken,
+
+    try {
+      const result = await authSevice.register(registerData);
+
+      setAuth(result);
+      navigate("/");
+      setSuccessMsg(`Успешна регистрация , Добре дошли!`);
+    } catch (error) {
+      setError(error.message);
     }
-    return (
-        <>
-            <AuthContext.Provider value={context}>
-                <AdsProvider>
+  };
 
-                    <Navigation />
-                    <main id='main-content'>
-                        {successMsg && (
-                            <Snackbar
-                                open={Boolean(successMsg)}
-                                autoHideDuration={6000}
-                                onClose={handleCloseMsg}
-                            >
+  const onLogout = async () => {
+    try {
+      await authSevice.logout();
+      setAuth({});
+    } catch (error) {
+      setAuth({});
+    }
+  };
+  const context = {
+    onLoginSubmit,
+    onRegisterSubmit,
+    onLogout,
+    userId: auth._id,
+    token: auth.accessToken,
+    userEmail: auth.email,
+    userName: auth.username,
+    isAuthenticated: !!auth.accessToken,
+  };
+  return (
+    <>
+      <AuthContext.Provider value={context}>
+        <AdsProvider>
+          <Navigation />
+          <main id="main-content">
+            {successMsg && (
+              <Snackbar
+                open={Boolean(successMsg)}
+                autoHideDuration={6000}
+                onClose={handleCloseMsg}
+              >
+                <Alert onClose={handleCloseMsg} severity="success">
+                  {successMsg}
+                </Alert>
+              </Snackbar>
+            )}
+            {error && (
+              <Snackbar
+                style={{
+                  height: "200px",
+                }}
+                open={Boolean(error)}
+                autoHideDuration={6000}
+                onClose={handleCloseMsg}
+              >
+                <Alert onClose={handleCloseMsg} severity="error">
+                  {error}
+                </Alert>
+              </Snackbar>
+            )}
 
-                                <Alert onClose={handleCloseMsg} severity="success">
-                                    {successMsg}
-                                </Alert>
-                            </Snackbar>
+            <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
 
-                        )}
-                        {error && (
-                            <Snackbar
-                                open={Boolean(error)}
-                                autoHideDuration={6000}
-                                onClose={handleCloseMsg}
-                                >
+              <Route path="/register" element={<Register />} />
+              <Route path="/catalog" element={<Catalog />} />
+              <Route path="/search" element={<Search />} />
 
-                                <Alert onClose={handleCloseMsg} severity="error">
-                                    {error}
-                                </Alert>
-                            </Snackbar>
+              <Route
+                path="/create-ad"
+                element={
+                  <AuthGuard>
+                    <CreateAd />
+                  </AuthGuard>
+                }
+              />
+              <Route path="/catalog/:adId" element={<AdDetails />} />
+              <Route
+                path="/catalog/:adId/edit"
+                element={
+                  <AuthGuard>
+                    <EditAd />
+                  </AuthGuard>
+                }
+              />
+              <Route
+                path="/profile/:profileId"
+                element={
+                  <AuthGuard>
+                    <ProfileGuard>
+                      <Profile />
+                    </ProfileGuard>
+                  </AuthGuard>
+                }
+              />
 
-                        )}
-
-                        <Routes>
-                            <Route path='/' element={<Hero />} />
-                            <Route path='/login' element={<Login />} />
-                            <Route path='/logout' element={<Logout />} />
-
-                            <Route path='/register' element={<Register />} />
-                            <Route path='/catalog' element={<Catalog />} />
-                            <Route path='/search' element={<Search />} />
-
-                            <Route path='/create-ad' element={<AuthGuard><CreateAd /></AuthGuard>} />
-                            <Route path='/catalog/:adId' element={<AdDetails />} />
-                            <Route path='/catalog/:adId/edit' element={<AuthGuard><EditAd /></AuthGuard>} />
-                            <Route path='/profile/:profileId' element={<AuthGuard><ProfileGuard><Profile /></ProfileGuard></AuthGuard>} />
-
-                            <Route path="*" element={<NotFound />} />
-
-                        </Routes>
-                    </main>
-                </AdsProvider>
-            </AuthContext.Provider>
-            <footer >
-
-                <Footer />
-            </footer>
-        </>
-    )
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+        </AdsProvider>
+      </AuthContext.Provider>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
